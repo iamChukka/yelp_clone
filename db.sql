ef0e038a066d398513a0d2251a1b29e0df2a32b8
Chukwuka Eluchie <eluchiechukwuka@yahoo.co.uk>
2021-03-16 16:31:33 +0100

    Connecting Express to Postgres Table

---
+++ db.sql
@@ -0,0 +1,18 @@
+-- products table
+
+CREATE TABLE products(
+    id INT,
+    name VARCHAR(50),
+    price INT,
+    on_sale BOOLEAN
+);
+
+CREATE TABLE restaurants(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    price_range INT NOT NULL CHECK(price_range >= 1 AND price_range <=5)
+);
+
+INSERT INTO restaurants (name , location, price_range)
+VALUES ('Mr Dees','Lekki', 4);
--- server/server.js
+++ server/server.js
@@ -1,14 +1,69 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const express = require("express");
+const db = require("./db");
 
-
+const morgan = require('morgan');
 const app = express();
 
-app.get("/getRestaurants",(req,res)=>{
-    res.send("These are the restaurants");
-});
+app.use(express.json());
 
+
+// Get all Restaurants
+app.get("/api/v1/restaurants", async (req,res)=>{
+
+    const results = await db.query('SELECT * FROM restaurants');
+    console.log(results);
+    res.status(200).json({
+        status: "success",
+        data:{
+            restaurants: ["Mr Dees", "Sweet Sensations"]
+        },
+    });
+});
+// Create a Restaurant
+app.post("/api/v1/restaurants", (req,res)=>{
+    console.log(req.body);
+
+    res.status(201).json({
+        status: "success",
+        data:{
+            restaurant: "Mr Dees"
+        }
+    });
+})
+
+//Update Retaurants
+app.put("/api/v1/restaurants/:id", (req,res)=>{
+    console.log(req.params.id);
+    console.log(req.body);
+
+    res.status(200).json({
+        status: "success",
+        data:{
+            restaurant: "Mr Dees"
+        }
+    });
+})
+
+//Get individual Restaurants
+app.get("/api/v1/restaurants/:id",(req,res)=>{
+    console.log(req.params);
+
+    res.status(200).json({
+        status: "success",
+        data:{
+            restaurant: "Mr Dees"
+        }
+    });
+})
+//Delete Restaurant
+app.delete("/api/v1/restaurants/:id",(req,res)=>{
+    
+    res.status(204).json({
+        status: "success",
+    });
+})
 const port  = process.env.PORT|| 3001;
 app.listen(port,()=>{
     console.log(`server is up and listening on port ${port}`)
